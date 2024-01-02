028b92b6607c0ec4774d595eb8f5f974d5487d8f
Carson Hartley <chartley1988@gmail.com>
2024-01-01 17:16:08 -0700

    Added content collection for Services. Nav and service grid pull from it.

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+
+// 2. Define a `type` and `schema` for each collection
+const servicesCollection = defineCollection({
+	type: "data", // v2.5.0 and later
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			picture: image().optional(),
+		}),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+	services: servicesCollection,
+};
