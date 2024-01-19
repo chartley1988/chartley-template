e7c7e2abe84234a88b9ab019f2357e65027a4ad4
Carson Hartley <chartley1988@gmail.com>
2024-01-18 21:57:59 -0700

    Add an "About Us" Page.
    It's mostly complete, but still needs a little bit more work.

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,43 +3,56 @@ import { z, defineCollection } from "astro:content";
 
 // 2. Define a `type` and `schema` for each collection
 const servicesCollection = defineCollection({
-	type: "data", // v2.5.0 and later
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			picture: image().optional(),
-			content: z
-				.array(
-					z.object({
-						heading: z.string().optional(),
-						text: z.string(),
-						image: image().optional(),
-					})
-				)
-				.optional(),
-		}),
+  type: "data", // v2.5.0 and later
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      picture: image().optional(),
+      content: z
+        .array(
+          z.object({
+            heading: z.string().optional(),
+            text: z.string(),
+            image: image().optional(),
+          }),
+        )
+        .optional(),
+    }),
 });
 
 const blogCollection = defineCollection({
-	type: "content", // v2.5.0 and later
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			url: z.string(),
-			summary: z.string(),
-			date: z.date(),
-			thumbnail: z
-				.object({
-					image: image(),
-					alt: z.string(),
-				})
-				.optional(),
-		}),
+  type: "content", // v2.5.0 and later
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      url: z.string(),
+      summary: z.string(),
+      date: z.date(),
+      thumbnail: z
+        .object({
+          image: image(),
+          alt: z.string(),
+        })
+        .optional(),
+    }),
+});
+
+// Team Members - Used in About Us page
+const teamMembersCollection = defineCollection({
+  type: "data", // v2.5.0 and later
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      role: z.string(),
+      picture: image().optional(),
+      content: z.string().optional(),
+    }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-	services: servicesCollection,
-	blog: blogCollection,
+  services: servicesCollection,
+  blog: blogCollection,
+  team: teamMembersCollection,
 };
